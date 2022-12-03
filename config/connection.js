f105f4a58356a85fdd66ca70126eea860d2403e8
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-02 20:58:59 -0600

    added changes to files

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ const sequelize = process.env.JAWSDB_URL
       host: 'localhost',
       dialect: 'mysql',
       port: 3306,
-      password: 'Grandzaria123$',
+      password: '',
       dialectOptions: {
         decimalNumbers: true,
       },

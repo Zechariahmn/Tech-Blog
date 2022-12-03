2bce9deffd6c057fd86817c7d78b1c6b771f14de
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-03 16:07:50 -0500

    added changes

--- server.js
+++ server.js
@@ -9,27 +9,25 @@ const hbs = exphbs.create({
     helpers
 });
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: process.env.DB_SECRET,
-    cookie: {
-      maxAge: 60 * 60 * 1000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
-    },
+    cookie: {},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-      db: sequelize
+        db: sequelize,
+        checkExpirationInterval: 1000 * 60 * 10, // will check every 10 minutes
+        expiration: 1000 * 60 * 30 // will expire after 30 minutes
     })
   };
   
+const app = express();
+const PORT = process.env.PORT || 3001;
+
   app.use(session(sess));
   
   // Sets handlebars as html to be served
